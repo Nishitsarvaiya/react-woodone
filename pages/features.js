98e08fd0e68d44ee8bba83111e6460ef0b9284a6
nishit <nishit@avdevs.com>
2021-12-17 18:51:33 +0530

    features page

--- pages/features.js
+++ pages/features.js
@@ -1,9 +1,89 @@
+import Image from 'next/image';
 import { Header } from '../components';
+import Feature1 from '../assets/images/desk-2.jpg';
+import Feature2 from '../assets/images/desk-5.jpg';
+import Feature3 from '../assets/images/desk-3.jpg';
 
 const Features = () => {
     return (
         <main>
             <Header />
+            <section className='features-section'>
+                <div className='features'>
+                    <div className='feature'>
+                        <div className='feature-image'>
+                            <Image src={Feature1} alt='' layout='fill' />
+                        </div>
+                        <div className='feature-content'>
+                            <div className='content'>
+                                <div className='content-number'>
+                                    <p>
+                                        <span>01</span> / 03
+                                    </p>
+                                </div>
+                                <div className='content-title'>
+                                    <h1>Oak Wood</h1>
+                                </div>
+                                <div className='content-text'>
+                                    <p>
+                                        Minimalist and stylish high quality desks <br />
+                                        made of real oak wood to make your <br />
+                                        workspace modern and elegant.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='feature'>
+                        <div className='feature-content'>
+                            <div className='content'>
+                                <div className='content-number'>
+                                    <p>
+                                        <span>02</span> / 03
+                                    </p>
+                                </div>
+                                <div className='content-title'>
+                                    <h1>Height Adjustment</h1>
+                                </div>
+                                <div className='content-text'>
+                                    <p>
+                                        Minimalist and stylish high quality desks <br />
+                                        made of real oak wood to make your <br />
+                                        workspace modern and elegant.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='feature-image'>
+                            <Image src={Feature2} alt='' layout='fill' />
+                        </div>
+                    </div>
+                    <div className='feature'>
+                        <div className='feature-image'>
+                            <Image src={Feature3} alt='' layout='fill' />
+                        </div>
+                        <div className='feature-content'>
+                            <div className='content'>
+                                <div className='content-number'>
+                                    <p>
+                                        <span>03</span> / 03
+                                    </p>
+                                </div>
+                                <div className='content-title'>
+                                    <h1>Several Sizes</h1>
+                                </div>
+                                <div className='content-text'>
+                                    <p>
+                                        Minimalist and stylish high quality desks <br />
+                                        made of real oak wood to make your <br />
+                                        workspace modern and elegant.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
     );
 };
