638752250e1fd518d184eb2404f00aa8deda7246
nishitsarvaiya <nishitsarvaiiya@gmail.com>
2022-02-23 22:03:04 +0530

    minor changes

--- components/header/header.js
+++ components/header/header.js
@@ -1,42 +1,47 @@
 import gsap from 'gsap';
+import Link from 'next/link';
 import { useEffect } from 'react';
 import { CustomLink } from '..';
 
 const Header = ({ page }) => {
-    useEffect(() => {
-        gsap.fromTo(
-            ['.logo h1', '.link', '.socials a', '.hamburger'],
-            1,
-            { y: '100%', opacity: 0 },
-            { y: '0', opacity: 1, stagger: 0.1 }
-        );
-    }, []);
+	useEffect(() => {
+		gsap.fromTo(
+			['.logo h1', '.link', '.socials a', '.hamburger'],
+			1,
+			{ y: '100%', opacity: 0 },
+			{ y: '0', opacity: 1, stagger: 0.1 }
+		);
+	}, []);
 
-    return (
-        <header data-scroll-section>
-            <nav>
-                <div className='logo'>
-                    <h1>W</h1>
-                </div>
-                {page !== 'home' ? (
-                    <>
-                        <div>
-                            <a href='#' className='link' data-cursor-target data-cursor-stick>
-                                <span>hello@woodone.co</span>
-                            </a>
-                        </div>
-                        <div className='socials'>
-                            <CustomLink>Facebook</CustomLink>
-                            <CustomLink>Instagram</CustomLink>
-                        </div>
-                    </>
-                ) : (
-                    ''
-                )}
-                <div></div>
-            </nav>
-        </header>
-    );
+	return (
+		<header data-scroll-section>
+			<nav>
+				<div className='logo' data-cursor-target data-cursor-stick>
+					<h1>
+						<Link href='/'>
+							<a>W</a>
+						</Link>
+					</h1>
+				</div>
+				{page !== 'home' ? (
+					<>
+						<div>
+							<a href='#' className='link' data-cursor-target data-cursor-stick>
+								<span>hello@woodone.co</span>
+							</a>
+						</div>
+						<div className='socials'>
+							<CustomLink>Facebook</CustomLink>
+							<CustomLink>Instagram</CustomLink>
+						</div>
+					</>
+				) : (
+					''
+				)}
+				<div></div>
+			</nav>
+		</header>
+	);
 };
 
 export default Header;
